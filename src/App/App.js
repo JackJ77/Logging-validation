e5adb1276bbe38f425442a6357279f13ebba9c44
JackJ77 <ku56@hotmail.com>
2018-05-19 14:49:57 +0200

    moved styles to Mbox

--- src/App/App.js
+++ src/App/App.js
@@ -3,25 +3,17 @@ import './App.css';
 import authCheck from './../utilities/authCheck';
 import MessageBox from './../MessageBox/MessageBox';
 
-const style = {
-  success: {
-    backgroundColor: '#bdffb5',
-    color: '#468840',
-    borderLeft: '2px solid #468840'
-  },
-  warning: {
-    backgroundColor: '#ffe649',
-    color: '#887002',
-    borderLeft: '2px solid #887002'
-  }
-}
-
 class App extends Component {
   state = {
     email: '',
     password: '',
     message: '',
-    style: {}
+    style: ''
+  }
+
+  componentDidMount() {
+    this.regEmail = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.regPassword = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.{6,})/;
   }
 
   onInputChange = (e) => {
@@ -32,20 +24,18 @@ class App extends Component {
     if(event) {
       event.preventDefault();
     }
-    const regEmail = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const regPassword = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.{6,})/;
 
-    if (regEmail.test(this.state.email) && regPassword.test(this.state.password)) {
+    if (this.regEmail.test(this.state.email) && this.regPassword.test(this.state.password)) {
       let valid = authCheck(this.state.email, this.state.password);
       if (valid) {
-        this.setState({message: 'login successful', style: style.success});
+        this.setState({message: 'login successful', style: 'success'});
       } else {
-        this.setState({message: 'invalid email or password',  style: style.warning});
+        this.setState({message: 'invalid email or password',  style: 'warning'});
       }
-    } else if (!regEmail.test(this.state.email)) {
-      this.setState({message: 'invalid email', style: style.warning});
-    } else if (!regPassword.test(this.state.password)) {
-      this.setState({message: 'invalid password', style: style.warning});
+    } else if (!this.regEmail.test(this.state.email)) {
+      this.setState({message: 'invalid email', style: 'warning'});
+    } else if (!this.regPassword.test(this.state.password)) {
+      this.setState({message: 'invalid password', style: 'warning'});
     }
   }
 
@@ -55,17 +45,21 @@ class App extends Component {
           {this.state.message === 'login successful' ? null :
           <form onSubmit={this.handleSubmit} className="App">
               <label htmlFor="email">Email</label>
-              <input value={this.state.email}
-                    onChange={ this.onInputChange }
-                    type="text"
-                    name="email"
-                    id="email" />
+              <input
+                placeholder="test@test.pl"
+                value={this.state.email}
+                onChange={ this.onInputChange }
+                type="text"
+                name="email"
+                id="email" />
               <label htmlFor="password">Password</label>
-              <input value={this.state.password}
-                    onChange={ this.onInputChange }
-                    type="password"
-                    name="password"
-                    id="password" />
+              <input
+                placeholder="Password1"
+                value={this.state.password}
+                onChange={ this.onInputChange }
+                type="password"
+                name="password"
+                id="password" />
               <label className="container" htmlFor="remember">Remember me</label>
               <input type="checkbox" name="remember" id="remember" />
               <button type="submit">Login</button>
--- src/MessageBox/MessageBox.js
+++ src/MessageBox/MessageBox.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './MessageBox.css';
 
+const style = {
+  success: {
+    backgroundColor: '#bdffb5',
+    color: '#468840',
+    borderLeft: '2px solid #468840'
+  },
+  warning: {
+    backgroundColor: '#ffe649',
+    color: '#887002',
+    borderLeft: '2px solid #887002'
+  }
+}
+
 const MessageBox = (props) => {
+  const messageStyle = props.style === 'warning' ? style.warning : style.success;
+
   return(
-    <div style={props.style} className="MessageBox">
-      {props.message}
-    </div>
+      <div style={messageStyle} className="MessageBox">
+        {props.message}
+      </div>
   );
 };
 
